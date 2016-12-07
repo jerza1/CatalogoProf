66fd7a1248a8243a25dbf7c9142291763c4bb91b
jerza <fco.j.bautista@hotmail.com>
2016-12-07 03:44:52 -0600

    Falta el Design responsive :P

--- js/grafAlum.js
+++ js/grafAlum.js
@@ -1,119 +1,134 @@
-var cv = document.getElementById('lienzo');
-ctx = cv.getContext('2d');
-var anio = document.getElementById('anio');
+window.addEventListener("load",function() {
+	var cv = document.getElementById('lienzo');
+	ctx = cv.getContext('2d');
+	var anio = document.getElementById('lista');
 
-function miFuncion(ruta,fun,myArr2) {
-	var xmlhttp = cargarXMLHttp();
-	xmlhttp.onreadystatechange = function() {
-		if(this.readyState == 4 && this.status == 200) {
-			try {
-				var myArr = JSON.parse(this.responseText);
-				fun(myArr,myArr2);
-			}catch(e) {
-				console.error(e);
-			}
-		}else {
-			console.warn('Hubo problemas');
-		}
-	};
-	xmlhttp.open("GET", ruta, true);
-	xmlhttp.send();
-}
-function carrera(carrera,myArr2) {
-    /*carr.addEventListener("change",function(){
-		miFuncion("json/grupo.json",desplegar,carrera);
-	});*/
-	anio.addEventListener("change",function(){
-		miFuncion("json/grupo.json",desplegar,carrera);
-	});
-}
-function desplegar(alumn,carrera){
-	var total = [];
-	var tam = carrera.length;
-	for (var i = 0; i < tam; i++) {
-		total[i] = 0;
-	}
-	for (var i = 0; i < alumn.length; i++) {
-		for (var j = 0; j < tam; j++) {
-			if(alumn[i].carrera_id == carrera[j].id && alumn[i].anio == anio.value){
-				total[j] += alumn[i].alumnos; 
-			}
-		}
-	}
-	console.log(total);
-    ctx.fillStyle = "white";
-    ctx.fillRect(0,0,cv.width,cv.height);
-    ctx.fillStyle = "black";
-    ctx.moveTo(0,cv.height-20);
-	ctx.lineTo(cv.width ,cv.height-20);
-	ctx.stroke();
-	num = 0;
-	for (var i = 0; i < 33; i++) {
-		ctx.fillText(num,29*i,cv.height-10);
-		num+=20;
-	}
-	ctx.font = "12px Arial";
-	ctx.fillText(carrera[0].nombre,10,10);
-	for (var i = 1; i < total.length; i++) {
-		ctx.fillText(carrera[i].nombre,10,(18.5*i)+10);
-	}
-	ctx.fillStyle = "rgba(0,0,255,0.4)";
-	ctx.fillRect(0,0,total[0]*1.4,12);
-	for (var i = 1; i < total.length; i++) {
-		ctx.fillRect(0,(18.5*i),total[i]*1.4,12);
-	}
-}
-window.addEventListener("load",function(){
-	ctx.moveTo(0,cv.height-20);
-	ctx.lineTo(cv.width ,cv.height-20);
-	ctx.stroke();
-	num = 0;
-	for (var i = 0; i < 33; i++) {
-		ctx.fillText(num,29*i,cv.height-10);
-		num+=20;
-	}
 	miFuncion("json/carrera.json",carrera,0);
 
-
-});
-function cargarXMLHttp() {
-	if(window.XMLHttpRequest){
-		xmlhttp = new XMLHttpRequest();
-	}else{
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	try {
-	xmlhttp.responseType="msxml-document"   //Microsoft XML Core Services 
-	} 
-	catch(err) {
-	} // Helping IE
-	return xmlhttp;
-}
-function e() {
-	nArg = arguments.length;
-	if(nArg > 0){
-		var elemt = document.createElement(arguments[0]);
-		for(var i = 1 ; i < nArg; i++){
-			var argm = arguments[i];
-			var tipo = typeof(argm);
-			if(tipo == "string"){
-				var texto = document.createTextNode(argm);
-					elemt.appendChild(texto);
+	function miFuncion(ruta,fun,myArr2) {
+		var xmlhttp = cargarXMLHttp();
+		xmlhttp.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200) {
+				try {
+					var myArr = JSON.parse(this.responseText);
+					fun(myArr,myArr2);
+				}catch(e) {
+					console.error(e);
+				}
+			}else {
+				console.warn('Hubo problemas');
 			}
-			else if(argm != '[object HTMLUnknownElement]'){
-				for (attr in argm) {
+		};
+		xmlhttp.open("GET", ruta, true);
+		xmlhttp.send();
+	}
+
+	function carrera(carrera,myArr2) {
+		miFuncion("json/grupo.json",desplegar,carrera);
+	}
+
+	function desplegar(alumn,carrera) {
+		var total = [];	var nombres = []; var tam = carrera.length;
+
+		for (var j = 0; j < tam; j++) {
+			if( carrera[j].tipo == 1){
+				nombres.push(carrera[j].nombre);
+				for (var i = 0,cont = 0; i <  alumn.length; i++) {
+					if(alumn[i].periodo == 'A' || alumn[i].periodo == 'B'){
+						if(alumn[i].carrera_id == carrera[j].id && alumn[i].anio == anio.value){
+							cont += alumn[i].alumnos; 
+						}
+					}
+				}
+				total.push(cont);
+			}
+		}
+
+		cuadricula();
+
+		ctx.font = "14px Arial";
+		ctx.fillText(nombres[0],10,15);
+		for (var i = 1; i < nombres.length; i++) {
+			ctx.fillText(nombres[i],10,(32*i)+15);
+		}
+
+		ctx.fillStyle = "rgba(0,0,255,0.4)";
+		ctx.fillRect(0,0,(total[0]/2)*2,20);
+		for (var i = 1; i < total.length; i++) {
+			ctx.fillRect(0,(32*i),(total[i]/2)*2,20);
+		}
+
+		anio.addEventListener("change",function() {
+			miFuncion("json/grupo.json",desplegar,carrera);
+		});
+	}
+
+	function cargarXMLHttp() {
+		if(window.XMLHttpRequest){
+			xmlhttp = new XMLHttpRequest();
+		}else{
+			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		try {
+		    xmlhttp.responseType="msxml-document"   //Microsoft XML Core Services 
+		}
+		catch(err) {
+	    } // Helping IE
+	    return xmlhttp;
+	}
+
+	function e() {
+		nArg = arguments.length;
+		if(nArg > 0){
+			var elemt = document.createElement(arguments[0]);
+			for(var i = 1 ; i < nArg; i++){
+				var argm = arguments[i];
+				var tipo = typeof(argm);
+				if(tipo == "string"){
+					var texto = document.createTextNode(argm);
+					elemt.appendChild(texto);
+				}
+				else if(argm != '[object HTMLUnknownElement]'){
+					for (attr in argm) {
 						if(typeof(argm[attr]) == "function"){
 							elemt.addEventListener(attr, argm[attr]);
 						}else{
-    						elemt.setAttribute(attr,argm[attr]);
-    					}
+							elemt.setAttribute(attr,argm[attr]);
+						}
 					}
+				}
+				else{
+					elemt.appendChild(argm);	
+				}	
 			}
-			else{
-				elemt.appendChild(argm);	
-			}	
+
 		}
-						
-	}
 		return elemt;
-}
+	}
+
+	function cuadricula() {
+		borrar();
+		ctx.save();
+		ctx.font = "12px Arial";
+		ctx.moveTo(0,cv.height-20);
+		ctx.lineTo(cv.width ,cv.height-20);
+		ctx.stroke();
+		var num = 20;
+		ctx.fillText(0,1,cv.height-10);
+		ctx.beginPath();
+		for (var i = 1; i < 17; i++) {
+			ctx.fillText(num,(40*i)-12,cv.height-10);
+			ctx.moveTo(i*40,0);
+			ctx.lineTo(i*40,cv.height-20);
+			num+=20;
+		}
+		ctx.stroke();
+		ctx.restore();
+	}
+
+	function borrar(argument) {
+		ctx.fillStyle = "white";
+		ctx.fillRect(0,0,cv.width,cv.height);
+		ctx.fillStyle = "black";
+	}
+});
